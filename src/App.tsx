885b5418e29ddd79d46246b257ab3d6e6c254c22
diver <impulsediver@gmail.com>
2025-05-22 18:35:57 +0900

    App.tsx 초기화 + 위치 변경

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Text, useColorScheme, View } from "react-native";
+
+/** */
+export function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === "dark";
+
+  /*
+   * To keep the template simple and small we're adding padding to prevent view
+   * from rendering under the System UI.
+   * For bigger apps the recommendation is to use `react-native-safe-area-context`:
+   * https://github.com/AppAndFlow/react-native-safe-area-context
+   *
+   * You can read more about it here:
+   * https://github.com/react-native-community/discussions-and-proposals/discussions/827
+   */
+  const safePadding = "5%";
+
+  return (
+    <View>
+      <Text>Hello World!</Text>
+    </View>
+  );
+}
